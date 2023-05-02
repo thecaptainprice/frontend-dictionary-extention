39afde75f92b929293ee10760165066a67bee285
Captain Price <reza.irisdev@gmail.com>
2023-05-02 23:25:44 +0330

    make toolbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ButtonComponent } from './components/button/button.component';
-import { SvgIconComponent } from './icon/component/svg-icon/svg-icon.component';
 import { IconModule } from './icon/icon.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, ToolbarComponent],
   imports: [CommonModule, IconModule],
-  exports: [ButtonComponent, IconModule],
+  exports: [ButtonComponent, IconModule, ToolbarComponent],
 })
 export class CoreModule {}
