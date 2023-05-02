502705889bd044b0fdcb9aca4e38dc2cafcc725c
Captain Price <reza.irisdev@gmail.com>
2023-05-03 01:25:28 +0330

    fix

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,23 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { IconModule } from './icon/icon.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { BottomNavComponent } from './components/bottom-nav/bottom-nav.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
-  declarations: [ButtonComponent, ToolbarComponent],
+  declarations: [
+    ButtonComponent,
+    ToolbarComponent,
+    BottomNavComponent,
+    AboutComponent,
+  ],
   imports: [CommonModule, IconModule],
-  exports: [ButtonComponent, IconModule, ToolbarComponent],
+  exports: [
+    ButtonComponent,
+    IconModule,
+    ToolbarComponent,
+    BottomNavComponent,
+    AboutComponent,
+  ],
 })
 export class CoreModule {}
