983ce294186d5547c86c1fd6764a50aa48445c75
Captain Price <reza.irisdev@gmail.com>
2023-05-02 23:26:42 +0330

    modify svg types

--- src/app/core/icon/icon.module.ts
+++ src/app/core/icon/icon.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SvgIconComponent } from './component/svg-icon/svg-icon.component';
 
-
-
 @NgModule({
-  declarations: [
-    SvgIconComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SvgIconComponent],
+  imports: [CommonModule],
+  exports: [SvgIconComponent],
 })
-export class IconModule { }
+export class IconModule {}
