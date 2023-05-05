dd650344b8fb12fb6e21ac7d0cd02d61049af6a4
Captain Price <reza.irisdev@gmail.com>
2023-05-05 12:25:43 +0330

    add lietner-box module and add word components

---
+++ src/app/core/lietner-box/components/add-word/add-word.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-word',
+  templateUrl: './add-word.component.html',
+  styleUrls: ['./add-word.component.sass']
+})
+export class AddWordComponent {
+
+}
---
+++ src/app/core/lietner-box/lietner-box.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddWordComponent } from './components/add-word/add-word.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddWordComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LietnerBoxModule { }
